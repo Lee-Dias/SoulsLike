b4caa8c8ef5a63f2b6d7a7693c7dabff11ea2041
Pninja12 <pauloalmeida1288@gmail.com>
2025-12-17 15:51:30 +0000

    Bonfire Canva working not completed

--- Assets/Temporary/Scripts/Bonfire.cs
+++ Assets/Temporary/Scripts/Bonfire.cs
@@ -1,6 +1,8 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using static Unity.VisualScripting.Metadata;
 
 public class BonfireWorldChanger : MonoBehaviour
 {
@@ -8,12 +10,53 @@ public class BonfireWorldChanger : MonoBehaviour
     [SerializeField] private string sceneToLoad = "SampleSceneDark"; // Change this or set in Inspector
     [SerializeField] private GameObject bonfireUI;
     private bool playerInside = false;
+    private RectTransform loadingImageRectTransform;
+    private GameObject loadingImageGameObject;
+
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        Transform loadingImageTransform = bonfireUI.transform.Find("LoadingImage");
+        loadingImageGameObject = bonfireUI.transform.Find("LoadingImage").gameObject;
+
+        if (loadingImageTransform != null)
+        {
+            loadingImageRectTransform = loadingImageTransform as RectTransform;
+            // Now use childYouWant however you need
+            // Example: childYouWant.SetActive(true);
+        }
+        else
+        {
+            Debug.LogError("Child with name 'LoadingImage' not found under bonfireUI!");
+        }
+    }
 
     // Call this method to load the scene (e.g., from a button click, collision, etc.)
     public void LoadTargetScene()
     {
+        loadingImageGameObject.SetActive(true);
+        StartCoroutine(GrowAndRotateOverTime(20));
+    }
+
+    private IEnumerator GrowAndRotateOverTime(float duration) //isto tem de ser alterado
+    {
+        float growAmountPerSecond = 200f;             // how much wider/taller it gets per second
+        float rotationSpeed = 360f + 180f;
+        float elapsedTime = 0f;// degrees per second (positive = clockwise)
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+
+            loadingImageRectTransform.sizeDelta = loadingImageRectTransform.sizeDelta + new Vector2(growAmountPerSecond, growAmountPerSecond) * Time.deltaTime;
+
+            loadingImageRectTransform.rotation = Quaternion.Euler(0, 0, loadingImageRectTransform.eulerAngles.z + rotationSpeed * Time.deltaTime);
+
+            yield return null;
+        }
         SceneManager.LoadScene(sceneToLoad);
     }
+
     public void TurnOn(InputAction.CallbackContext ctx)
     {
         if (!ctx.performed) return;
