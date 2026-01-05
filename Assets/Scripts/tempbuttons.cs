b73fed855598adf60c013c604e2d1368cade0e5d
JoaoCarvalho2002 <jonitkick@gmail.com>
2026-01-05 18:51:48 +0000

    Fixed menu buttons

---
+++ Assets/Scripts/tempbuttons.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+public class tempbuttons : MonoBehaviour
+{
+   public void Startbutton()
+   {
+       UnityEngine.Debug.Log("checkbuttons");
+   }
+}
