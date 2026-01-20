908dace32bc7e5058e05db1f3d2638d58c6b96d9
Pninja12 <pauloalmeida1288@gmail.com>
2026-01-20 15:43:30 +0000

    Volume Control is working

--- Assets/Scripts/SettingsManager.cs
+++ Assets/Scripts/SettingsManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class SettingsManager : MonoBehaviour
 {
@@ -7,6 +8,7 @@ public class SettingsManager : MonoBehaviour
     GameObject closeButton;
     GameObject settingsMenu;
     GameObject settingsOptions;
+    Slider volumeSlider;
     
     
     PlayerController playerController;
@@ -16,16 +18,39 @@ public class SettingsManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //------------UI Management------------//
         background = transform.GetChild(0).gameObject;
         closeButton = transform.GetChild(1).gameObject;
         settingsMenu = transform.GetChild(2).gameObject;
         settingsOptions = transform.GetChild(3).gameObject;
         playerController = FindFirstObjectByType<PlayerController>();
+
+        for (int i = 0; i < settingsOptions.transform.childCount; i++)
+        {
+            if (settingsOptions.transform.GetChild(i).name == "VolumeSlider")
+            {
+                volumeSlider = settingsOptions.transform.GetChild(i).GetComponent<Slider>();
+            }
+        }
+        //------------End of UI Management------------//
+
+        //------------Volume Management------------//
+        if(!PlayerPrefs.HasKey("MainVolume"))
+        {
+            PlayerPrefs.SetFloat("MainVolume", 1f);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+        //------------End of Volume Management------------//
     }
 
     // Update is called once per frame
     void Update()
     {
+        //------------UI Management------------//
         if (on)
         {
             background.SetActive(true);
@@ -49,8 +74,10 @@ public class SettingsManager : MonoBehaviour
         {
             TurnOff();
         }
+        //------------End of UI Management------------//
     }
 
+    //------------UI Management Method------------//
     public void OpenCloseStettingsInputPlayer(InputAction.CallbackContext ctx)
     {
         if(inOptions) 
@@ -92,4 +119,25 @@ public class SettingsManager : MonoBehaviour
         background.SetActive(false);
         closeButton.SetActive(false);
     }
+    //------------End of UI Management Method------------//
+
+    //------------Volume Management Method------------//
+    public void MainVolumeControl(System.Single volume)
+    {
+        AudioListener.volume = volume;
+        Save(volume);
+    }
+
+    void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("MainVolume");
+    }
+
+    void Save(System.Single volume)
+    {
+        PlayerPrefs.SetFloat("MainVolume", volume);
+    }
+    //------------End of Volume Management Method------------//
+
+
 }
