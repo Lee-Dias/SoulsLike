ebe45abf7adeee458797e13bbad19528c2ab760e
Pninja12 <pauloalmeida1288@gmail.com>
2026-03-12 14:23:11 +0000

    On a temporary ManagerScene added a trail giver to the sword, still it doesn't have a on or off when attacking

---
+++ Assets/Scripts/TrailAttack.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class TrailAttack : MonoBehaviour
+{
+    [SerializeField] string nameOfChild;
+    [SerializeField] AnimationCurve widthCurve;
+    [SerializeField] float timeOfTrail;
+    [SerializeField] float minVertexDistanceOfTrail;
+    [SerializeField] int cornerVerticesOfTrail;
+    [SerializeField] Material materialOfTrail;
+    List<GameObject> listOfChildren= new List<GameObject>();
+    TrailRenderer trailRenderer;
+    GameObject childWithTrail;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartCoroutine(FindChild());
+    }
+
+    IEnumerator FindChild()
+    {
+        while(childWithTrail == null)
+        {
+            yield return null;
+            for(int i = 0; i < listOfChildren.Count; i++)
+            {
+                if (listOfChildren[i].transform.Find(nameOfChild) != null)
+                {
+                    childWithTrail = listOfChildren[i].transform.Find(nameOfChild).gameObject;
+                    break;
+                }    
+            }
+
+            List<GameObject> tempListOfChildren = new List<GameObject>();
+            foreach(GameObject child in listOfChildren)
+            {
+                if(child.transform.childCount > 0)
+                {
+                    tempListOfChildren.Add(child.transform.GetChild(0).gameObject);
+                }
+            }
+            listOfChildren = tempListOfChildren;
+
+            if(listOfChildren.Count == 0)
+            {
+                yield return null;
+                listOfChildren = new List<GameObject>();
+                while(true)
+                {
+                    yield return null;
+                    if(transform.childCount == 0)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        listOfChildren.Add(transform.GetChild(0).gameObject);
+                        break;    
+                    }
+                    
+                }
+            }
+            
+        }
+
+        childWithTrail.AddComponent<TrailRenderer>();
+        trailRenderer = childWithTrail.GetComponent<TrailRenderer>();
+        trailRenderer.time = timeOfTrail;
+        trailRenderer.materials = new Material[1] { materialOfTrail };
+        trailRenderer.widthCurve = widthCurve;
+        trailRenderer.minVertexDistance = minVertexDistanceOfTrail;
+        trailRenderer.numCornerVertices = cornerVerticesOfTrail;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
